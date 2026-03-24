23afffc1a8fe8dc5bdf2556c35969477eed97b26
RANNIEL MUNOZ DE LA CRUZ <raniel.ant0880@gmail.com>
2026-03-24 13:33:34 -0400

    Update ALLOWED_HOSTS in settings.py
    Removed '127.0.0.1' from ALLOWED_HOSTS for production.

--- PortafolioWed/settings.py
+++ PortafolioWed/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-+3dwn5r#b2oqw8og$k_muei3y)87i4#4x@+@8(ed6y-e0+)9vd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['rannieldev.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['rannieldev.pythonanywhere.com']
 
 
 
